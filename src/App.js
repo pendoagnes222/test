0608e302c540cb76b23ef47555f212541b156c47
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2023-01-24 09:04:20 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+import Navbar from './components/Navbar/Navbar';
+
+
+function App() {
+  return (
+    <div className='App'> 
+    <Navbar />
+    <h1>hi</h1>
+    <h2>low</h2>
+    
+    
+    
+    
+    </div>
+  );
+}
+
+export default App;
